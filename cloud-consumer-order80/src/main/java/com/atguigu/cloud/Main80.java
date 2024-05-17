1dd5a92254afb23419fe07a6b4668e69db56b1eb
kliest <1265506926@qq.com>
2024-05-17 21:49:02 +0800

    Initial commit

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/cloud/Main80.java
@@ -0,0 +1,12 @@
+package com.atguigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class Main80 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main80.class,args);
+    }
+}
